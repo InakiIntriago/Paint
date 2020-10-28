77df082f95779633c0eba1c16926972352cb3c83
Diego Manzanarez <dieguirri@Diegos-MacBook-Pro.local>
2020-10-28 11:52:46 -0600

    Se agregaron las funciones de rectangle, triangle y el color Dark Orange

--- Paint.py
+++ Paint.py
@@ -34,9 +34,28 @@ def drawcircle(start, end):
     
 def rectangle(start, end):
     "Draw rectangle from start to end." 
+    up()                               
+    goto(start.x,start.y)             
+    down()                             
+    begin_fill()                       
+    for count in range(2):            
+        forward(end.x-start.x)        
+        left(90)                      
+        forward(end.y-start.y)        
+        left(90)
+    end_fill()
+
 
 def triangle(start, end):
     "Draw triangle from start to end."
+    up()                               
+    goto(start.x,start.y)             
+    down()                             
+    begin_fill()
+    for count in range(3):            
+        forward(end.x-start.x)        
+        left(120)   
+    end_fill()
 
 def tap(x, y):
     "Store starting point or draw shape."
@@ -65,6 +84,7 @@ onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
 onkey(lambda: color('purple'), 'P')
+onkey(lambda: color('dark orange'), 'D')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', drawcircle), 'c')
